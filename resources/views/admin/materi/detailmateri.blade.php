f7cd7e83be68d561bd00668ae537caf2fb58a095
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-05-02 14:42:29 +0700

    feat: Rev all data

--- resources/views/admin/materi/detailmateri.blade.php
+++ resources/views/admin/materi/detailmateri.blade.php
@@ -84,7 +84,7 @@
                                             <div class="modal-dialog modal-lg">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="exampleModalLabel{{ $item->id }}">Ubah {{ $item->nama_materi }}</h5>
+                                                        <h5 class="modal-title" id="exampleModalLabel{{ $item->id }}">Lihat Gambar {{ $item->nama_materi }}</h5>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
                                                     </div>
                                                     <div class="modal-body">
